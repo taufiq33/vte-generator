04b5779803ae6362675cc2997ecf39ccc2579329
taufiq33 <taufiqhidayat1908@gmail.com>
2019-10-12 14:23:35 +0700

    ubah gitignore dan config,

--- GUI/config.py
+++ GUI/config.py
@@ -1,3 +1,3 @@
-generated_file_path = '/home/taufiq/Documents/python/code/python3/xlsxwriter/GUI/generated_file/'
+generated_file_path = '/home/taufiq/Documents/python/code/python3/xlsxwriter/vte-generator/GUI/'
 kadaluarsa = '12/27/2050 12:44:03'
         
